cd721356d86bf3c1ede94a61385a5533bc31daf2
Adityo Deshmukh <adie@infinitelybeta.com>
2012-06-07 18:38:58 +0530

    Added my stuff

--- packs/user/init.el
+++ packs/user/init.el
@@ -1,2 +1,11 @@
-(setq user-full-name "Baishampayan Ghose")
-(setq user-mail-address "b.ghose@helpshift.com")
+(setq user-full-name "Adityo Deshmukh")
+(setq user-mail-address "adie@helpshift.com")
+
+(setq ring-bell-function 'ignore)
+
+;; ----------------
+;; Mac Stuff
+;; ----------------
+(global-set-key (kbd "M-RET") 'ns-toggle-fullscreen)
+
+(set-default-font "Andale Mono-12")
